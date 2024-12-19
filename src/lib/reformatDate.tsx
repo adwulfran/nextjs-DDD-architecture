3a915f9e23bc0512838a89fc3a2855509cf55a4b
Adrien Wulfran <adrienwulfran@gmail.com>
2024-12-19 18:38:58 +0100

    Remove and rename utils files

--- src/lib/reformatDate.tsx
+++
@@ -1,4 +0,0 @@
-export function reformatDate(dateString: string): string {
-  const [month, day, year] = dateString.split('/');
-  return `${day}/${month}/${year}`; // Return the new format (DD/MM/YYYY)
-}
