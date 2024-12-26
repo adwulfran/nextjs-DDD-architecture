3e9cc21040e9c50647325cd60153bd884ab50730
Adrien Wulfran <adrienwulfran@gmail.com>
2024-12-26 15:29:52 +0100

    Remove useless html. Replace css file with tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 
-import { getServerSession } from 'next-auth';
 import EventsSearch from '../components/events/Search';
 import EventsList from '../components/events/List';
 import EventsFilter from '../components/events/Filters';
 import styles from './styles.module.css'
 import { Suspense } from 'react';
 import Skeleton from '@mui/material/Skeleton';
-import { Button } from '../components/button';
 
 export interface ISearchQuery {
   title?: string;
@@ -18,15 +16,11 @@ export interface ISearchQuery {
 export default async function Page({ searchParams }: {
   searchParams: Promise<ISearchQuery>
 }) {
-  const session = await getServerSession();
   const query = await searchParams;
 
   return (
     <>
-      <div className={styles.main}>
-        {session?.user?.name}
-        <Button><span>Logout </span></Button>
-        <h1>Events</h1>
+      <div className="flex flex-col items-center gap-[2vh] p-3" >
         <EventsSearch />
         <EventsFilter />
         <Suspense fallback={<Skeleton variant="rectangular" width={210} height={60} />}>
